00bf4961627446ea326d8790765950805e32ec9f
panfilo macantan <olifnapnatnacam27.com>
2025-07-15 14:17:56 +0800

    remove KEY in PUBLISHABLE

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { ClerkProvider } from '@clerk/clerk-react';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE;
 
 if (!PUBLISHABLE_KEY) {
 	throw new Error('Missing Publishable Key');
